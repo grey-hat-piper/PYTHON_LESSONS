133e35708ee1e8d0ab75acc9ded61e3a8dca0269
princederek <marteyderek08@gmail.com>
2025-09-27 03:59:52 +0000

    week1_assignment

---
+++ assignment/intro.py
@@ -0,0 +1,23 @@
+first_num = float(input('Enter your first number:'))
+second_num = float(input('Enter second number:'))
+opp = input('Mathematical operation:')
+
+#Addition
+if opp == '+':
+    print(first_num + second_num)
+
+#Subtraction
+elif opp == '-':
+    print(first_num - second_num)
+
+#Multiplication
+elif opp == '*':
+    print(first_num * second_num)
+
+#Division
+elif opp == '/':
+    print(first_num / second_num)
+
+#NONE
+else:
+    print('Operation not found!')
