77243f846d863c55aec2c49d275e986921caaf9f
princederek <marteyderek08@gmail.com>
2025-09-28 12:23:31 +0000

    data structures assignment

---
+++ assignment/data_structures.py
@@ -0,0 +1,26 @@
+#Empty list
+my_list = []
+
+#Appending items
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#Inserting 15 at second position
+my_list.insert(1,15)
+
+#Extending list
+my_list.extend([50,60,70])
+
+#Removing last element
+my_list.pop()
+
+#Sorting list in ascending order
+my_list.sort()
+
+#Print the index of 30
+print(my_list.index(30))
+
+
+print(my_list)
